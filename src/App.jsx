4e476dbcc06ade094004648463a9b7c00fef229d
Axel Palomino <axel_palomino10@hotmail.com>
2024-03-18 05:34:04 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,11 @@ export const App = () => {
       <TabCategories />
       <section className="py-10 text bg-red-500">
         <div className="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-         <CardProduct/>
-         <CardProduct/>
-         <CardProduct/>
-         <CardProduct/>
-         <CardProduct/>
+        {
+          products.map((product)=>(
+            <CardProduct product={product}/>
+          ))
+        }
          
         </div>
       </section>
